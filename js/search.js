772c932b375e649d0d64caf9bf6a70ab8aef50b2
Coconuteagle <131874183+Coconuteagle@users.noreply.github.com>
2024-09-18 23:01:48 +0900

    Update search.js
    검색 시작 할 때 hide 제거

--- js/search.js
+++ js/search.js
@@ -1,38 +1,39 @@
 const search = document.getElementById("search");
-// 검색 입력할 시 다른 페이지 넘어가도 기존 검색어 유지
 
+// 검색 입력할 시 다른 페이지 넘어가도 기존 검색어 유지
 function searcher() {
   let search = document.getElementById("search").value.toLowerCase();
   let content_box = document.getElementsByClassName("content_box");
+
+  // 검색 시작 시 모든 content_box에서 hide 제거
   for (let i = 0; i < content_box.length; i++) {
-    if (!content_box[i].classList.contains("hide")) {
-      let question = content_box[i].getElementsByClassName("question");
-      if (question[0].innerHTML.toLowerCase().indexOf(search) != -1) {
-        content_box[i].style.display = "block";
-        // content_box[i].classList.remove("hide");
-      } else {
-        content_box[i].style.display = "none";
-        // content_box[i].classList.add("hide");
-      }
-      if (search === "") {
-        $(".content_box").removeAttr("style");
-      }
+    content_box[i].classList.remove("hide");
+  }
+
+  // 검색어에 맞지 않는 항목을 숨기고, 맞는 항목은 보이게 처리
+  for (let i = 0; i < content_box.length; i++) {
+    let question = content_box[i].getElementsByClassName("question");
+    if (question[0].innerHTML.toLowerCase().indexOf(search) !== -1) {
+      content_box[i].style.display = "block";
+    } else {
+      content_box[i].style.display = "none";
+    }
+  }
+
+  // 검색어가 없을 경우 모든 content_box를 보이게 설정
+  if (search === "") {
+    for (let i = 0; i < content_box.length; i++) {
+      content_box[i].style.display = "block";
     }
   }
 }
+
 function hideTopbar() {
   $("#search-box").css({ height: "0" });
   $(".search_box").css({ bottom: "-150px" });
   $("#container").css({ "margin-top": "170px" });
   $("#question_mark__container").hide(300);
 }
-// function showTopbar() {
-//   $("#search-box").css({ height: "440px" });
-//   $(".search_box").removeAttr("style");
-//   $("#container").removeAttr("style");
-//   $("#question_mark__container").show(300);
-//   moveToTop();
-// }
 
 $(window).scroll(function () {
   let scrollTop = $(window).scrollTop();
