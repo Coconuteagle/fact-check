aa5a40c1d98f137c61332fd3b3b44560ee1b57c7
Coconuteagle <131874183+Coconuteagle@users.noreply.github.com>
2024-10-07 20:48:44 +0900

    Update checkbox.js
    다시 돌아옴 김대인으로

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,101 +1,36 @@
-$(document).ready(function() {
-  // Toggle next question visibility on content box click
-  $(".content_box").click(function () {
-    $(this).children(".next_quest").toggle(300);
-  });
-
-  // Function to toggle visibility based on checkbox
-  function checkbox_function() {
-    const checkboxId = $(this).attr("id").split("--")[0];
-    if ($(this).attr("id") === `${checkboxId}--checkbox`) {
-      const toggling = $(`.${checkboxId}`);
-      toggling.each(function () {
-        $(this).toggleClass("hide");
-        $(this).toggle();
-      });
-    }
-  }
-
-  // Function to toggle checked menu item
-  function is_checked() {
-    const iCheck = $(this).parent().children("i");
-    iCheck.toggle(500);
-    const menu = $(this).parent()[0];
-    menu.classList.toggle("menu__checked");
-  }
-
-  // Event listeners for checkboxes using 'change' event
-  $("input[type='checkbox']").on("change", is_checked);
-  $("input[type='checkbox']").on("change", checkbox_function);
-  // Removed hideTopbar from checkbox events
-
-  // Search input element
-  const $search = $("#search");
-
-  // Function to hide top bar
-  function hideTopbar() {
-    $("#search-box").css({ height: "0" });
-    $(".search_box").css({ bottom: "-150px" });
-    $("#container").css({ "margin-top": "170px" });
-    $("#question_mark__container").hide(300);
-  }
-
-  // Function to update checkboxes based on search input
-  function updateCheckboxesBasedOnSearch() {
-    let searchValue = $search.val().toLowerCase();
-    let $checkboxes = $("input[type='checkbox']");
-
-    console.log("updateCheckboxesBasedOnSearch called with:", searchValue);
-
-    if (searchValue !== "") {
-      // Check all checkboxes if not already checked
-      $checkboxes.each(function() {
-        if (!$(this).prop('checked')) {
-          console.log("Checking checkbox:", $(this).attr('id'));
-          $(this).prop('checked', true).trigger('change');
-        }
-      });
-    } else {
-      // Uncheck all checkboxes if checked
-      $checkboxes.each(function() {
-        if ($(this).prop('checked')) {
-          console.log("Unchecking checkbox:", $(this).attr('id'));
-          $(this).prop('checked', false).trigger('change');
-        }
-      });
-    }
-  }
-
-  // Event listeners for search input
-  $search.on("click", hideTopbar);
-  $search.on("keyup", function() {
-    updateCheckboxesBasedOnSearch();
-  });
-
-  // Window scroll event
-  $(window).scroll(function () {
-    let scrollTop = $(window).scrollTop();
-    const searchBoxHeight = $("#search-box").prop("style").height;
-    if (searchBoxHeight) {
-      if (scrollTop > 0) {
-        $search.css({ position: "fixed", top: "10px", bottom: "auto" });
-        $(".menu_list").css({ position: "fixed", top: "80px" });
-        $("#empty_box").show();
-      } else {
-        $search.removeAttr("style");
-        $(".menu_list").removeAttr("style");
-        $("#empty_box").hide();
-      }
-    } else {
-      if (scrollTop > 400) {
-        $search.css({ position: "fixed", top: "10px", bottom: "auto" });
-        $(".menu_list").css({ position: "fixed", top: "80px" });
-        $("#empty_box").show();
-      } else {
-        $search.removeAttr("style");
-        $(".menu_list").removeAttr("style");
-        $("#empty_box").hide();
-      }
-    }
-  });
+$(".content_box").click(function () {
+  $(this).children(".next_quest").toggle(300);
 });
+
+function checkbox_function() {
+  const checkboxId = $(this).attr("id").split("--")[0];
+  if ($(this).attr("id") === `${checkboxId}--checkbox`) {
+    const toggling = $(`.${checkboxId}`);
+    for (i = 0; i < toggling.length; i++) {
+      if (toggling[i].classList.contains("hide")) {
+        toggling[i].classList.remove("hide");
+        toggling[i].style.display = "block";
+      } else {
+        toggling[i].classList.add("hide");
+        toggling[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function is_checked() {
+  const iCheck = $(this).parent().children("i");
+  iCheck.toggle(500);
+  const menu = $(this).parent()[0];
+  if (menu.classList.contains("menu__checked")) {
+    menu.classList.remove("menu__checked");
+  } else {
+    menu.classList.add("menu__checked");
+  }
+}
+
+$("input[type='checkbox']").on("click", is_checked);
+$("input[type='checkbox']").on("click", checkbox_function);
+$("input[type='checkbox']").on("click", searcher);
+$("input[type='checkbox']").on("click", moveToSearchbar);
+$("input[type='checkbox']").on("click", hideTopbar);
